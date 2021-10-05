58c6b1ffe423a91dad8e7df0845f1d2997e7d7fa
jakovsch <jakovsch@gmail.com>
2021-10-05 11:43:33 +0200

    i18n: english

--- agrobot/i18n/locales/en_EN.py
+++ agrobot/i18n/locales/en_EN.py
@@ -1,4 +1,26 @@
 
 en_EN = {
-    
+    'embed_title': 'Now playing',
+    'embed_dur': 'Duration',
+    'embed_req': 'Requested by',
+    'embed_pub': 'Published by',
+    'embed_lnk': 'Link',
+    'embed_lnk_txt': 'here',
+
+    'error_generic': 'Here\'s an error:',
+    'error_no_dm': 'You can\'t use this in DMs.',
+    'error_no_bot_voice': 'I\'m not in a voice channel, what now?',
+    'error_no_user_voice': 'You are not in a voice channel...',
+    'error_in_bot_voice': 'I\'m already in a voice channel...',
+    'error_int_range': 'Do you know how percentages work??',
+
+    'status_not_playing': 'Nothing is playing rn.',
+    'status_enqueued': 'Here goes:',
+    'status_volume': 'Volume set to',
+
+    'queue_empty': 'Empty queue, lmao.',
+    'queue_title': 'The Queue:',
+    'queue_prev': 'Previous:',
+    'queue_page': 'Page',
+    'queue_none': 'Nothing!'
 }
